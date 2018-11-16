41b372d941c07bbf19b6537e45bd1663a26a4b3f
Shaun Stone <shauns@nutmeg.com>
2018-11-16 12:37:43 +0000

    removing unsupported flag for docker

--- src/targets/chrome/docker.js
+++ src/targets/chrome/docker.js
@@ -127,7 +127,7 @@ function createChromeDockerTarget({
     ensureDependencyAvailable('docker');
     const args = runArgs
       .concat([
-        '--shm-size=1g',
+        // '--shm-size=1g',
         '-p',
         `${port}:${port}`,
         chromeDockerImage,
